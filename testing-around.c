52b8510e05a7c17484ad044894e7de8096e2044a
1Kill2Steal <imliterallyhim@duck.com>
2023-10-02 22:57:12 +0300

    a

--- testing-around.c
+++ testing-around.c
@@ -1,37 +1,5 @@
 #include <stdio.h>
 
-#define OUT     0
-#define IN      1
-
 int main() {
-    int c, i, counter, wordCounter, state;
-    int wordLength[100];
-
-    i = counter = wordCounter = 0;
-    state = OUT;
-
-    while ((c = getchar()) != EOF) {
-	
-        if (c == ' ' || c == '\t' || c == '\n') { 
-            state = OUT;
-            --counter;
-        }
-	    else if (state == OUT) {
-            state = IN;
-	        wordLength[wordCounter] = counter;
-            counter = 0;
-	        wordCounter++;	    
-	    }
-    
-        ++counter;
-
-    }
-    
-    wordLength[wordCounter] = counter;
-
-    printf("\nword lengths:\n");
-    for(i = 1; i <= wordCounter; ++i) {
-        printf("Word %d length: %d\n", i, wordLength[i]);
-    }
     
 }
