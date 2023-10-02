3c32e33f5485adba00337f18b123d60d86d07499
1Kill2Steal <imliterallyhim@duck.com>
2023-10-02 23:14:59 +0300

    a

--- testing-around.c
+++ testing-around.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    
+    const char * z = "testing one two three";
+    int m;
+    int charcount;
+
+    charcount = 0;
+    for(m=0; z[m]; m++) {
+        if(z[m] != ' ') {
+            charcount ++;
+        }
+    }
 }
