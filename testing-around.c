06569580499ac89c4869fa37c406be852cde7cf4
1Kill2Steal <imliterallyhim@duck.com>
2023-10-05 08:21:59 +0300

    As of 05/10/2023

--- testing-around.c
+++ testing-around.c
@@ -3,9 +3,52 @@
 // this file is only for writing and testing
 // code, finished projects are elsewhere,
 
-//currently working on:
+//currently working on: 030-array-of-symbols.c
+
+//defines the longest length of the char array.
+#define MAXLINE		1000
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
 
 int main()
 {
-    
+	int len;
+	int max;
+	char line[MAXLINE]; //current line
+	char longest[MAXLINE]; //longest line
+	max = 0;
+	while ((len = getline(line, MAXLINE)) > 0)
+		if (len > max)
+		{
+			max = len;
+			copy(longest, line);
+		}
+	
+	if (max > 0)
+		printf("%s", longest);
+	
+	return 0;
+}
+
+int getline(char s[], int lim)
+{
+	int c, i;
+
+	for (i=0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n')
+	{
+		s[i] = c;
+		++i;
+	}
+}
+
+void copy(char to[], char from[])
+{
+	int i;
+	
+	i = 0;
+	while((to[i] = from[i]) != '\0')
+		++i;
 }
