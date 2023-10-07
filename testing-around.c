e608995e11b4cf91c4ed10514e88225192b6d2e3
1Kill2Steal <imliterallyhim@duck.com>
2023-10-07 21:44:05 +0300

    As of 07.10.2023

--- testing-around.c
+++ testing-around.c
@@ -3,38 +3,60 @@
 // currently working on: 032-1.17
 
 #include <stdio.h>
+#include <string.h>
 
-//defines the minimum required line length to print the text
+//defines the minimum required line length to print the text:
 #define MINLINELEN	80
 
-//defines the total max length of the array
-#define MAXARRLEN	100000
+//max possible user input size:
+#define BUFFER		1024
+
+int moreThanMinChars(char *line, int count);
+void processLines();
 
 int main()
 {
-	int c, i, j;
-	i = j = 0;
-	char line[MAXARRLEN];
+	printf("Enter text:\n");
 
-	while((c = getchar()) != EOF)
-	{
-		if(c != '\n')
-		{
-			++i;
-		}
-		else if(c == '\n' && i != 0)
-		{
-			++i;
-			if(i > MINLINELEN)
-			{
-				for(j; j < i; j++)
-					line[j] = c;
-			}
-			i = j = 0;
-		}
-	}
-	printf("Lines that have more length than %d:", MINLINELEN);
-	for(i=0; i < MAXARRLEN; i++)
-		printf("%c", line[i]);
+	processLines();
+
+	return 0;
 }
 
+int moreThanMinChars(char *line, int count)
+{
+	return count > MINLINELEN;
+}
+
+void processLines()
+{
+	char line[BUFFER];
+	char allLines[BUFFER][BUFFER];
+	int allLineCount = 0;
+	int count = 0;
+	int c;
+	
+	while((c = getchar()) != EOF)
+	{
+		if(count < sizeof(line) - 1)
+		{
+			line[count] = c;
+			count++;
+		}
+		
+		if (c == '\n')
+		{
+			line[count] = '\0';
+			if(moreThanMinChars(line, count))
+			{
+				strcpy(allLines[allLineCount], line);
+				allLineCount++;
+			}
+			count = 0;
+		}
+	}
+	for(int i = 0; i < allLineCount; i++)
+	{
+		printf("%s\n", allLines[i]);
+	}
+}
