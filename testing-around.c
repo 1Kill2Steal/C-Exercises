0be6bf009cafa1c75c6500335d0141391e3a79dc
1Kill2Steal <imliterallyhim@duck.com>
2023-10-12 11:33:09 +0300

    As of 11.10.2023

--- testing-around.c
+++ testing-around.c
@@ -4,8 +4,42 @@
 
 #include <stdio.h>
 
+#define BUFFER				1024
+
+int getLine(int curLine[], int max);
+
 int main()
 {
-
+	int input[BUFFER];
+	int read = getLine(input, BUFFER);
+	for(int i = 0; i < read; i++)
+		printf("%c", input[i]);
 	return 0;
+}
+
+int getLine(int curLine[], int max)
+{
+	int i = 0;
+	int j = 0;
+	int output = 0;
+	int line[max];
+	int c;
+	while((c = getchar()) != EOF && i < max)
+	{
+		if(c == '\n' && i > 0)
+		{
+			for(i; i>=0; i--, j++)
+			{
+				curLine[j] = line[i];
+			}
+			output += j;
+		}
+		line[i] = c;
+		i++;
+	}
+	
+	if(output < max)
+		return output; 
+	else
+		return printf("BUFFA OVERFLOW M8");
 }
